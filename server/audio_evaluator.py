83e90e6d573d226bb224fd2783ba405c5f947528
Aryan Kasliwal <aryankasliwal@CPN-MAC-M1-001.local>
2023-04-04 10:33:45 +0800

    Added audio evaluator

---
+++ server/audio_evaluator.py
@@ -0,0 +1,17 @@
+from mido import MidiFile
+from mido import tick2second
+
+def evaluate_generated_track(midi_track_original, midi_track_generated):
+    original_scores = []
+    for track in midi_track_original:
+        original_scores.append(track)
+        
+    index = 0
+    similarity_score, total_score = 0, 0
+    for track in midi_track_generated:
+        if track == original_scores[index]:
+            similarity_score += 1
+        total_score += 1
+        index += 1
+    print(f"{similarity_score * 100 / total_score}%")
+    return (similarity_score * 100 / total_score)
