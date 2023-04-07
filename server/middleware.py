5220a0ff0294c0694978fc113796b69b648693fd
KASLIWAL Aryan Girish <akasliwal2@ad.cityu.edu.hk>
2023-04-07 17:51:08 +0800

    Finalised middleware and server scripting

--- server/middleware.py
+++ server/middleware.py
@@ -14,6 +14,9 @@ CORS(app)
 PORT = 8080
 
 music_generated = False
+generation_dir = r"server/generated_test"
+zip_dir = "./Generated_files"
+rnn_checkpoint_dir = r'server/drums_rnn/logdir/final_run'
 
 @app.route("/upload", methods=['POST'])
 def upload():
@@ -29,7 +32,7 @@ def upload():
         # Generate drums from the received file
         model_generate_drums(filename)
 
-        # Delete file after generation
+        # Delete received file after generation
         delete_file(filename)
 
     return 200
@@ -38,7 +41,7 @@ def upload():
 @app.route("/get_names", methods=["GET"])
 def get_names():
     file_names = []
-    for files in os.listdir(r"server/generated_test"):
+    for files in os.listdir(generation_dir):
         file_names.append(files)
     return jsonify({
         'ok': True,
@@ -49,30 +52,10 @@ def get_names():
 
 @app.route("/download", methods=['GET'])
 def send_files():
-    # print("Get request called")
-    # path = r"server\generated_test"
-    # #return send_from_directory(directory=path, path="")
-    # new_path = r"server\temp"
-    file_name = ""
-    # for file in os.listdir(path):
-    #     file_name = file
-    #     print(file_name)
-    #     path += rf"\{file}"
-    #     old_path = path
-    #     if not os.path.exists(new_path):
-    #         os.mkdir(new_path)
-    #     for existing_files in os.listdir(new_path):
-    #         os.remove(new_path + rf"\{existing_files}")
-    #     new_path += rf"\{file}"
-    #     shutil.move(old_path, new_path)
-    #     break
-    zip_path = r"..\Generated_files.zip"
-    if os.path.exists(zip_path):
-        os.remove(zip_path)
-    shutil.make_archive("Generated_files", 'zip', "./generated_test")
-    # print(send_path)
+    zip_path = r".\Generated_files.zip"
+    delete_file(zip_path)
+    shutil.make_archive(zip_dir, 'zip', generation_dir)
     return send_file(zip_path, mimetype='application/zip', as_attachment=True, download_name="Generated_tracks.zip")
-    # return send_from_directory(directory=r".\generated_test", filename="")
 
 
 def delete_file(path):
@@ -80,13 +63,16 @@ def delete_file(path):
         os.remove(path)
 
 
+def delete_directory(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
 def model_generate_drums(input_file):
     number_steps = 128
-    run_dir = r'server\drums_rnn\logdir\final_run'
-    output_dir=r'server\generated_test'
+    run_dir = rnn_checkpoint_dir
+    output_dir=generation_dir
 
-    if os.path.exists(output_dir):
-        shutil.rmtree(output_dir)
+    delete_directory(output_dir)
 
     # number_steps decides the length of the track.
     # Start with a low value and keep increasing it until 
@@ -102,16 +88,6 @@ def model_generate_drums(input_file):
     global music_generated
     music_generated = True
 
-# Handler = CustomHTTPRequestHandler
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#    print("serving at port", PORT)
-#    httpd.serve_forever() 
-
-# if __name__ == "__main__":
-#     model_generate_drums(input_file="C:/Users/akasliwal2/Documents/FYP/Final_Year_project/Dataset/FinalData/test/c/1_funk_80_beat_4-4_31.midi")
-
 
 if __name__ == "__main__":
     app.run(port=PORT, debug=True)
-    # send_files()
-    # get_names()
